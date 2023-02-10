9b09f6a7dc5fdf8ba215a2f0acffb36ccc308272
iddimsangi <adammsangi@gmail.com>
2023-02-10 14:11:28 +0300

    comment out neibor part of an api

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-const coutryFuc = (countryData) => {
-  const html = ` <article class="country">
+const coutryFuc = (countryData, classNameValue = "") => {
+  const html = ` <article class="country ${classNameValue}">
     <img class="country__img" src="${countryData.flags.png}" />
     <div class="country__data">
       <h3 class="country__name">${countryData.name.common}</h3>
@@ -26,11 +26,19 @@ const fetchCountryRender = (country) => {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      console.log(data[0].name.common);
-      console.log(data[0].currencies.EUR.name);
-      console.log(data[0].flags.png);
       coutryFuc(data[0]);
+      const neibor = data[0].borders[0];
+      console.log(neibor);
+      if (!neibor) return;
+      fetch(`https://restcountries.com/v3.1/alpha/${neibor}`)
+        .then((resp) => {
+          resp.json();
+          console.log(resp);
+        })
+        // .then((data) => {
+        //   console.log(data);
+        // //   coutryFuc(data, "neighbour");
+        // });
     });
 };
 fetchCountryRender("portugal");
